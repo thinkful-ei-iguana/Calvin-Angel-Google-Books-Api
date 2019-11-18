bb3c37e5e623ec76957914dbf45f888ef6a915a7
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-18 12:58:14 -0700

    stage 1 rebuild

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Calvin-Angel-Google-Books-Api
+
+- Provide a search form to allow users to search for books by some search term that they enter.
+- Allow filters such as type of book or whether the book is a free ebook or not.
+- Display the search result in list and optionally allow the user to click a result to see further  
+   details of the book.
+- Use the Google Books API to perform the search
---
+++ bookmarksapi/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//should have a search form, allow filters for 'type of book, free, etc
+//display the result in a list, optionally click a book to display full description, etc
+//must use google api to perform this search
+//header should be h1, have a large black bar surrounding it
+//just below header should be a grey box nav bar with 'search' with a submit button next to it
+//full description should be expandable, it should contain title, author, price, description of book
+
+serviceWorker.unregister();
