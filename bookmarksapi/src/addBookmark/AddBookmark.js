5f52e3b43dd1232d0a8e8da67499dde056eb0313
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-18 09:56:02 -0700

    Co-authored-by: aliceaa396 <ala31996@gmail.com>

--- bookmarksapi/src/addBookmark/AddBookmark.js
+++ bookmarksapi/src/addBookmark/AddBookmark.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import './addBookmark.css';
+import React, { Component } from "react";
+import "./addBookmark.css";
 
-class AddBookmark extends Component{
-  render(){
-    return(
+class AddBookmark extends Component {
+  render() {
+    return (
       <div className="addbookmark">
         <h2>Add Bookmark</h2>
         <form className="addbookmark__form">
           <label htmlFor="title">Title:</label>
-          <input type="text" name="title" id="title" placeholder="Title"/>
+          <input type="text" name="title" id="title" placeholder="Title" />
           <label htmlFor="url">Url:</label>
-          <input type="text" name="url" id="url" placeholder="url"/>
+          <input type="text" name="url" id="url" placeholder="url" />
           <label htmlFor="description">Description:</label>
-          <textarea name="description" id="description" placeholder="description"/>
+          <textarea
+            name="description"
+            id="description"
+            placeholder="description"
+          />
           <label htmlFor="rating">Rating: </label>
-          <input
-            type="number"
-            name="rating"
-            id="rating"
-            min="1"
-            max="5"/>
+          <input type="number" name="rating" id="rating" min="1" max="5" />
 
           <div className="addbookmark__buttons">
             <button>Cancel</button>
-            <button type="submit" >Save</button>
-          </div>  
+            <button type="submit">Save</button>
+          </div>
         </form>
       </div>
-
     );
   }
 }
---
+++ bookmarksapi/src/fab/fab.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./fab.css";
+
+class Fab extends Component {
+  render() {
+    return <div className="fab">&#43;</div>;
+  }
+}
+
+export default Fab;
