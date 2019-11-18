7d06f75f5cfc2b9adb06347be8b2536af30600b3
Angel Alicea <ala31996@gmail.com>
2019-11-18 11:27:11 -0500

    added

--- bookmarksapi/src/addBookmark/addBookmark.test.js
+++ bookmarksapi/src/addBookmark/addBookmark.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AddBookmark from './addBookmark';
+import AddBookmark from './AddBookmark';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
