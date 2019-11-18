01d18f8915da24bdb5e7943dc6019714e54327f6
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-18 10:45:49 -0700

    part way there, no npm issues now :)

--- bookmarksapi/src/App.js
+++ bookmarksapi/src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddBookmark from "./addBookmark/addBookmark";
+import BookmarkApp from "./bookmarkApp/bookmarkApp";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  render() {
+    const page = this.state.showAddForm ? (
+      <AddBookmark />
+    ) : (
+      <BookmarkApp bookmarks={this.state.bookmarks} />
+    );
+
+    return <div className="App">{page}</div>;
+  }
 }
 
 export default App;
